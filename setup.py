adc1f72b296b563dce3f0fdfe83d1256ce119486
Manuel <60146151+MQSchleich@users.noreply.github.com>
2022-02-28 20:44:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="continuous-template",
+    version="0.1",
+    description="A package supporting rapid python development with GitHub CI",
+    url="https://github.com/MQSchleich/continuous-template",
+    author="Peter Parker",
+    author_email="julian.m.kleber@gmail.com",
+    license="MIT",
+    packages=["continious-template"],
+    zip_safe=False,
+)
