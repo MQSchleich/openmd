66940043e101fdd49d295418b60373b6ca37e0a4
Manuel <manuel.schleich@github.com>
2022-03-01 17:21:46 +0100

    integrated

--- openmd/SImulatorLJ.py
+++ openmd/SImulatorLJ.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import h5py
 from Simulator import Simulator
 
 
@@ -140,10 +142,13 @@ class SimulatorLJ(Simlator):
         """
         # assert type(force_constants) == float, "force constant must be of type float but is"+str(type(float))
         positions, velocities = self.allocate_simulation()
+        # calculate chunks
         positions, velocities = self.integrator(
             positions=positions, velocities=velocities
         )
-        return (positions, velocities)
+        # needs to save to disk
+
+        # return (positions, velocities)
 
     def _integrator(self, positions, velocities):
 
@@ -188,8 +193,67 @@ class SimulatorLJ(Simlator):
     def _calc_accel():
         raise NotImplementedError
 
-    def _save_to_disk():
-        raise NotImplementedError
+    def save_to_disk():
+        # variables that needed
+        self.path = path
+        self.title = title
+        self.grid = grid
+        self.results = results
 
-    def _plot_results():
-        raise NotImplementedError
+        results_file = h5py.File(self.path + f"{self.title}.hdf5", "w")
+        rtset = results_file.create_dataset(
+            f"time", self.grid.shape, dtype="f", data=self.grid
+        )
+        rset = results_file.create_dataset(
+            f"{self.title}", self.results.shape, dtype="f", data=self.results
+        )
+        results_file.close()
+
+        # raise NotImplementedError
+
+    def plot_results():
+        # variables that needed
+        self.path = path
+        self.title = title
+        self.X_list_name = X_list_name
+        self.Y_list_name = Y_list_name
+        if results_name is not None:
+            self.data_name = results_name
+        self.figsize = figsize
+        self.dpi = dpi
+        self.grid = grid
+        self.results = results
+
+        """
+        Plots a graph given a list of 1D params and params name (a list of strings).
+        :params title:
+        :params N_list_name:
+        :params N_list:
+        :params params_name:
+        :params params:
+        :params Figsize: ( default = (15,5) )
+        :params Dpi:    ( default = 300 )
+        """
+
+        plt.rcParams["figure.figsize"] = self.figsize[0], self.figsize[1]
+        plt.figure()
+
+        for i in range(len(params)):
+
+            if len(self.grid.shape) == 1:  # shared
+                plt.plot(self.grid, self.results[i], label=str(self.result_name[i]))
+
+            if len(self.grid.shape) > 1:  # not shared
+                plt.plot(self.grid, self.results[i], label=str(self.results_name[i]))
+
+            plt.xlabel(str(self.X_list_name), fontsize=20)
+            plt.ylabel(str(self.Y_list_name), fontsize=20)
+            plt.legend(fontsize=15)
+            plt.xticks(fontsize=16)
+            plt.yticks(fontsize=16)
+
+            # plt.show()
+
+            plt.tight_layout()
+            plt.savefig(self.path + f"{self.title}.png", dpi=self.dpi)
+            plt.close()
