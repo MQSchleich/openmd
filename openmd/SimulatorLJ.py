e27e5c92b6a0d155874175b0eab15ed094ec6137
Manuel <manuel.schleich@github.com>
2022-03-02 23:58:24 +0100

    got the initial structure of the simulation done

--- openmd/SimulatorLJ.py
+++ openmd/SimulatorLJ.py
@@ -1,36 +1,40 @@
+from turtle import pos
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 from Simulator import Simulator
 
 
-class SimulatorLJ(Simlator):
+class SimulatorLJ(Simulator):
     def __init__(
         self,
         mass,
         sim_time: float,
         time_step: float,
         initial_values,
+        box_length,
         force=None,
         force_constants=[1, 1],
         integrator=None,
-        boundary_conditions=True,
+        periodic=True,
     ) -> None:
 
         self._mass = mass
         self._sim_time = sim_time
         self._time_step = time_step
-        self._num_steps = sim_time / time_step
+        self._num_steps = int(sim_time / time_step)
         self._initial_values = initial_values
         self._initial_pos = initial_values[0]
         self._initial_velocities = initial_values[1]
+        self._box_length = box_length
         if force is not None:
             self._force = force
         self._force_constants = force_constants
         if integrator is not None:
             self._integrator = integrator
+        self._periodic = periodic
 
-        @property()
+        @property
         def mass(self):
             return self._mass
 
@@ -42,7 +46,7 @@ class SimulatorLJ(Simlator):
         def mass(self):
             del self._mass
 
-        @property()
+        @property
         def sim_time(self):
             return self._sim_time
 
@@ -54,7 +58,7 @@ class SimulatorLJ(Simlator):
         def sim_time(self):
             del self._sim_time
 
-        @property()
+        @property
         def time_step(self):
             return self._time_step
 
@@ -66,7 +70,7 @@ class SimulatorLJ(Simlator):
         def time_step(self):
             del self._time_step
 
-        @property()
+        @property
         def initial_values(self):
             return self._initial_values
 
@@ -82,7 +86,7 @@ class SimulatorLJ(Simlator):
             del self._initial_positions
             del self._initial_velocities
 
-        @property()
+        @property
         def initial_pos(self):
             return self._initial_pos
 
@@ -94,7 +98,7 @@ class SimulatorLJ(Simlator):
         def initial_pos(self):
             del self._initial_pos
 
-        @property()
+        @property
         def initial_velocities(self):
             return self._initial_velocities
 
@@ -106,7 +110,19 @@ class SimulatorLJ(Simlator):
         def initial_velocities(self):
             del self._initial_velocities
 
-        @property()
+        @property
+        def box_length(self):
+            return self._box_length
+
+        @box_length.setter
+        def box_length(self, value):
+            self._integrator = value
+
+        @box_length.deleter
+        def box_length(self):
+            del self._box_length
+
+        @property
         def force(self):
             return self._force
 
@@ -118,7 +134,7 @@ class SimulatorLJ(Simlator):
         def force(self):
             del self._force
 
-        @property()
+        @property
         def integrator(self):
             return self._integrator
 
@@ -130,7 +146,20 @@ class SimulatorLJ(Simlator):
         def force(self):
             del self._integrator
 
-    def integration_wrapper(self):
+        @property
+        def periodic(self):
+            return self._periodic
+
+        @force.setter
+        def periodic(self, value):
+            assert type(value) is bool, "Did not assign boolean for self._periodic."
+            self._periodic = value
+
+        @force.deleter
+        def periodic(self):
+            del self._periodic
+
+    def simulate(self):
         """
         velocity verlet algorithm to integrate sympletic equations of motion up to 3D
         :param grid:
@@ -141,24 +170,37 @@ class SimulatorLJ(Simlator):
         :return:
         """
         # assert type(force_constants) == float, "force constant must be of type float but is"+str(type(float))
-        positions, velocities = self.allocate_simulation()
+        positions, velocities = self._allocate_simulation()
         # calculate chunks such that the for-loop from the integretor is written here and the integrator just called
-        positions, velocities = self.integrator(
-            positions=positions, velocities=velocities
-        )
+        for i in range(self._num_steps - 1):
+
+            positions, velocities = self._integrator(
+                positions=positions, velocities=velocities, iteration=i
+            )
+
         # needs to save to disk
 
-        # return (positions, velocities)
+        return (positions, velocities)
 
-    def _integrator(self, positions, velocities):
+    def _integrator(self, positions, velocities, iteration):
+        """Abstracted the integrator to the core algorithm because the loop happens in the war
 
-        acc = self.calc_accel(positions[:, :, 0], self.force_constants)
-        for i in range(0, self.num_steps - 1):
-            velocity_pr = velocities[:, :, i] + 0.5 * acc * self.time_step
-            positions[:, :, i + 1] = positions[:, :, i] + self.time_step * velocity_pr
-            # TODO implement boundary
-            acc = self.calc_accel(positions[:, :, i + 1], self.force_constants)
-            velocities[:, :, i + 1] = velocity_pr + 0.5 * acc * self.time_step
+        Args:
+            positions (_type_): _description_
+            velocities (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        i = iteration
+        acc = self._calc_accel(positions[:, :, 0], self._force_constants)
+        velocity_pr = velocities[:, :, i] + 0.5 * acc * self._time_step
+        positions[:, :, i + 1] = positions[:, :, i] + self._time_step * velocity_pr
+        if self._periodic == True:
+            positions[positions > self._box_length / 2] -= self._box_length
+            positions[positions <= -self._box_length / 2] += self._box_length
+        acc = self._calc_accel(positions[:, :, i + 1], self._force_constants)
+        velocities[:, :, i + 1] = velocity_pr + 0.5 * acc * self._time_step
         return [positions, velocities]
 
     def _apply_boundary():
@@ -172,26 +214,82 @@ class SimulatorLJ(Simlator):
             _type_: _description_
         """
 
-        initial_positions = self.initial_pos
-        initial_velocities = self.initial_velocities
+        initial_positions = self._initial_pos
+        initial_velocities = self._initial_velocities
         positions = np.zeros(
-            (initial_positions.shape[0], initial_positions.shape[1], self.num_steps)
+            (initial_positions.shape[0], initial_positions.shape[1], self._num_steps)
         )
         velocities = np.zeros(
-            (initial_velocities.shape[0], initial_velocities.shape[1], self.num_steps)
+            (initial_velocities.shape[0], initial_velocities.shape[1], self._num_steps)
         )
         positions[:, :, 0] = initial_positions
         velocities[:, :, 0] = initial_velocities
         return (positions, velocities)
 
-    def _calc_pairwise_distance():
-        raise NotImplementedError
+    def _calc_accel(self, positions, constants):
 
-    def _force():
-        raise NotImplementedError
+        forces = self._force(
+            positions, constants=constants, box_length=self._box_length
+        )
+        acc = forces / self._mass
+        return acc
 
-    def _calc_accel():
-        raise NotImplementedError
+    def _force(self, positions, constants, box_length):
+        """Standard LJ implementation with PBC. Does only return the force to introduce more modularity.
+
+        Args:
+            positions (_type_): 3D positions
+            constants (_type_): sigma, epsilon constants
+            box_length (_type_): length of the periodic box
+
+        Returns:
+            _type_:  array of forces
+        """
+        force = np.zeros((positions.shape[0], 3))
+        epsilon, sigma = constants
+        for i in range(len(force)):
+            difference = self._calc_pairwise_distance(
+                particle=i, positions=positions, box_length=box_length
+            )
+            force_t = -48 * epsilon * np.power(sigma, 12) / np.power(
+                difference, 13
+            ) - 24 * epsilon * np.power(sigma, 7) / np.power(difference, 7)
+            force[i, :] = force_t
+        return force
+
+    def _calc_pairwise_distance(self, particle, positions, box_length):
+        """Standard euclidean pairwise distance. Is abstracted to enable innovation here.
+
+        Args:
+            positions (_type_): 3D positions
+            box_length (_type_): box_length
+        """
+        particle = positions[particle, :]
+        difference = positions - particle
+        difference[difference > box_length / 2] -= box_length
+        difference[difference <= -box_length / 2] += box_length
+        difference = np.linalg.norm(difference, axis=0)
+        return difference
+
+    def _allocate_simulation(self):
+
+        """Pre allocating RAM for the integration
+
+        Returns:
+            _type_: _description_
+        """
+
+        initial_positions = self._initial_pos
+        initial_velocities = self._initial_velocities
+        positions = np.zeros(
+            (initial_positions.shape[0], initial_positions.shape[1], self._num_steps)
+        )
+        velocities = np.zeros(
+            (initial_velocities.shape[0], initial_velocities.shape[1], self._num_steps)
+        )
+        positions[:, :, 0] = initial_positions
+        velocities[:, :, 0] = initial_velocities
+        return (positions, velocities)
 
     def save_to_disk():
         # variables that needed
